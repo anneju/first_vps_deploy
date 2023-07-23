150414ce1fff45bde853f43d4d2c4a475d2eabe7
anneju <anneju.824@gmail.com>
2023-07-23 10:12:01 +0900

    add passenger path

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpack
 
 # Default value for default_env is {}
 set :default_env, {
-  'PATH' => "/usr/pgsql-11/bin:$PATH",
+  'PATH' => "/usr/pgsql-11/bin:/usr/local/passenger/passenger-6.0.18/bin:$PATH",
   'LD_LIBRARY_PATH' => "/usr/pgsql-11/lib:$LD_LIBRARY_PATH"
 }
 
@@ -40,3 +40,6 @@ set :default_env, {
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# 確保 Capistrano 可以正確訪問到 passenger
+set :passenger_cmd, '/usr/local/passenger/passenger-6.0.18/bin/passenger'
